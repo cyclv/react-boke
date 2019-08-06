6ceb89ce4eafa2e7b5528b47632f66d9675ececa
cyc123 <1014555699@qq.com>
2019-08-06 18:09:57 +0800

    20190806

--- src/view/index.js
+++ src/view/index.js
@@ -14,22 +14,24 @@ class Index extends Component{
                         <img src={logo} className="left" alt="logo" />
                         <div className="right flexfs">
                             <div className='list'><input onChange={(e)=>this.inputchange(e)} placeholder='输入该兴趣内容'></input> </div>
-                            <div className='list mobilenone flexfs'><div className=''>首页</div><div>文章</div><div>推荐</div></div>
+                            <div className='list mobilenone flexfs'><div className='navlist'>首页</div><div className='navlist'>文章</div><div className='navlist'>推荐</div></div>
                         </div>
                     </div>
                 </header>
                 <main className='boke-mian flexcolfs'>
                     {/* 中心内容 */}
-                    <div className='center flexlb'>
-                        <div className='left'>
+                    <div className='center flexcollb'>
+                        <div className='left flexcolct'>
                             <div className='boke-list'>
-                                <div className='info'>源码阅读(1)：Java中主要的List结构——概述</div>
-                                <div className='info'>
-    典型的数据结构中，对于“表”结构的定义是：在一维空间下元素按照某种逻辑结构进行线性连接排列的数据结构（一对一）。
-    java中集合定义中所包括的链表（ArrayList）、链表（LinkedList）、各种队列（Queue/Deque）、栈（Stack）等都满足这样的定义。
-    本文及后续的几篇文章中将介绍Java集合结构中关于List接口、Queue接口、Set接口下的重要实现类。                    
+                                <div>
+                                    <div className='info'>源码阅读(1)：Java中主要的List结构——概述</div>
+                                    <div className='info'>
+        典型的数据结构中，对于“表”结构的定义是：在一维空间下元素按照某种逻辑结构进行线性连接排列的数据结构（一对一）。
+        java中集合定义中所包括的链表（ArrayList）、链表（LinkedList）、各种队列（Queue/Deque）、栈（Stack）等都满足这样的定义。
+        本文及后续的几篇文章中将介绍Java集合结构中关于List接口、Queue接口、Set接口下的重要实现类。                    
+                                    </div>
+                                    <div className='info'>2019年 08月06日</div>
                                 </div>
-                                <div className='info'>2019年 08月06日</div>
                             </div>    
                         </div>
                         <div className='right'>
